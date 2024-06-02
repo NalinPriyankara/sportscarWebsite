565f7771a40bee64780137acbbd0f1a0fda2aa73
NalinPriyankara <112267206+NalinPriyankara@users.noreply.github.com>
2024-06-02 13:09:09 +0530

    new car

--- sportscarwebsite/src/pages/Banner.jsx
+++ sportscarwebsite/src/pages/Banner.jsx
@@ -29,15 +29,15 @@ function Banner() {
         cars.slice(0,6).map((car, index) => (
             <div key={car._id} className={`slide ${car.active ? 'active' : undefined}`}>
               <div className="container-fluid">
-                <div className="row banner top">
-                  <div className="col-lg-4 padding-0 banner-top-left">
+                <div className="row banner-top">
+                  <div className="col-lg-4 p-0 banner-top-left">
                     <div className="banner-title">
                       <h1>Get Your Dream {car.make}</h1>
                       <span className='slide-number'>0{car._id}</span>
                     </div>
                     <SlideBtn index = {index} slideChange = {handleSlideChange}/>
                   </div>
-                  <div className="col-lg-8 padding-0 banner-top-right">
+                  <div className="col-lg-8 p-0 banner-top-right">
                     <div className="banner-image">
                       <img 
                         src={car.bannerImg} 
@@ -53,7 +53,7 @@ function Banner() {
                     </div>
                   </div>
                 </div>
-                <div className="row banner bottom">
+                <div className="row banner-bottom">
                   <div className="col-lg-4 padding-0"></div>
                   <div className="col-lg-5 padding-0"></div>
                   <div className="col-lg-3 padding-0"></div>
